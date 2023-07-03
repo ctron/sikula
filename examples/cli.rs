987997943c2d3de2d4aa546432a20c21a7b89553
Jens Reimann <ctron@dentrassi.de>
2023-07-03 10:34:15 +0200

    feat: allow seeing the MIR level

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,8 +1,13 @@
+use sikula::lir;
+
 include!("../tests/resource.rs");
 
 fn main() {
     let query = std::env::args().skip(1).collect::<Vec<_>>().join(" ");
     println!("Input: '{query}'");
-    let query = DeriveResource::parse(&query).unwrap();
-    println!("{query:#?}");
+
+    let query = lir::parse_query(&query).expect("Failed to parse into MIR");
+    println!("\nMIR:\n{query:#?}");
+    let query = DeriveResource::parse_from(query).expect("Failed to parse into LIR");
+    println!("\nLIR:\n{query:#?}");
 }
